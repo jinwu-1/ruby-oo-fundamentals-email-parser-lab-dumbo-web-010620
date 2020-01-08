503bd2167624963052aa4145e8291836e7cce7a5
Jin Wu <jinn.wuu@gmail.com>
2020-01-08 02:00:30 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,13 +1,9 @@
 class EmailAddressParser
+  attr_accessor :email_address
   
-attr_accessor :email_address
-
- def initialize(email_address)
-   @email_address = email_address
- end
- 
- def parse 
-   @email_address.delete(",").split.uniq
- end
- 
+  def initialize(email_address)
+    @email_address = email_address
+  end
+  
+  
 end
